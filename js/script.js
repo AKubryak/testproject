98ce9bf2ce4e12defa060a16b7894d14fdc856fd
Aleksey <example@gmail.com>
2024-05-11 22:34:35 +0300

    check pass

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict"
+let userName = prompt('Your name', 'Admin');
+if (userName == 'Admin') {
+    let pass = prompt('Password', 'Я главный');
+    if (pass === 'Я главный') {
+        alert('Здравствуйте!');
+    } else if (pass === '' || pass === null) {
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+} else if (userName === '' || userName === null) {
+    alert('Отменено');
+} else {
+    alert('Я вас не знаю');
+}
